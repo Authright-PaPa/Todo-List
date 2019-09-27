899f00dbf50e55a9442de75c2eff535ed3862976
ar-hathaway <xinyi.zhang@authright.com>
2019-09-27 14:12:37 -0400

    Add autocomplete functionality in input field.

--- list.js
+++ list.js
@@ -1,7 +1,13 @@
 const newTask = document.getElementById("todo");
 const toDoUl = document.getElementById("ul-list");
 const completeUl = document.getElementById("complete-ul-list");
-
+let tasks = ["Buy Starbucks at 9am",
+"Feed bunnies",
+"Purchase 2 bottles of milk",
+"Contact Chuck for extra keys",
+"Car Inspectiion Check",
+"Go to gym",
+"Reorder BW's food & hay"];
 
 createNewTask = (task) => {
   if (newTask.value.length > 0){
@@ -31,7 +37,9 @@ createNewTask = (task) => {
 addTask = () => {
   console.log("Adding task to todo list...");
   const li = createNewTask(newTask.value);
-  toDoUl.appendChild(li); 
+  toDoUl.insertBefore(li,toDoUl.childNodes[0]); 
+  tasks.push(newTask.value);
+  // console.log(tasks);
   newTask.value="";
 
   bindIncompleteItems(li, completeTask);
@@ -69,7 +77,7 @@ function redoTask(){
   const redoBtn = li.querySelector(".redo");
   redoBtn.remove()
   
-  toDoUl.appendChild(li); 
+  toDoUl.insertBefore(li,toDoUl.childNodes[0]); 
   bindIncompleteItems(li, completeTask);
 };
 
@@ -97,12 +105,6 @@ bindCompleteItems = (taskItem, redoButtonPress) => {
   deleteButton.onclick = deleteTask;
 };
 
-// bindUndoItems = (taskItem, redoButtonPress) => {
-//   console.log("Binding the undo task to todo list...");
-//   const redoButton = taskItem.querySelector(".redo");
-//   redoButton.onclick = redoButtonPress;
-// };
-
 for(let i=0; i < toDoUl.children.length; i++) {
   bindIncompleteItems(toDoUl.children[i], completeTask);
 }
@@ -110,10 +112,6 @@ for(let i=0; i < toDoUl.children.length; i++) {
 for(let i=0; i < completeUl.children.length; i++) {
   bindCompleteItems(completeUl.children[i], redoTask);
 }
-
-// for(let i=0; i < completeUl.children.length; i++) {
-//   bindUndoItems(completeUl.children[i], redoTask);
-// }
 
 window.onload = function() {
     document.getElementById('todo').onkeypress = function searchKeyPress(event) {
@@ -173,117 +171,19 @@ const minutes = ('0' + today.getMinutes()).slice(-2);
 const now = month + ' ' + day;
 
 document.getElementById("time").innerHTML = "It's <span class='hour'>" + hours + ":" + minutes + "</span><br/><span class='date'>" + month + ' ' + day + sufix + ', ' + weekday + '.';
-// document.getElementById("weekday").innerHTML = weekday;
-// document.getElementById("date").innerHTML = now;
 
-
-// li.onclick = function() {this.parentNode.removeChild(this);}
-// const newTask = document.getElementById("todo");
-// const addTaskBtn = document.getElementById("addBtn");
-// const incompleteTask = document.getElementById("ul-list");
-// const completeTask = document.querySelector(".complete-list ul");
-
-// document.getElementById("addBtn").addEventListener("click", addTask);
-
-// function createNewTask(task) {
-//     if (newTask.value.length > 0){
-//         var li = document.createElement("li"); //<li>
-//         var checkBox = document.createElement("input"); //checkbox
-//         var label = document.createElement("label"); // <label>
-//         label.innerText = task;
-//         checkBox.type = "checkbox";
-
-//         li.setAttribute('class', 'li-list');
-//         li.appendChild(checkBox);
-//         li.appendChild(label);
-
-//         return li;
-//     }
-// }
-
-// function addItem() {
-//     if (newTask.value.length > 0){
-//         const todoItem = createNewTask(newTask.value);
-//         incompleteTask.appendChild(todoItem);
-//         //completeTask.appendChild(todoItem);
-//         newTask.value="";
-
-//         bindIncompleteItems(todoItem, createCompleteTask());
-//     };
-// };
-
-// // var completeTask = function() {
-// //     var listItem = this.parentNode;
-// //     var deleteBtn = document.createElement("button"); 
-// //     deleteBtn.innerText = "Delete";
-// //     deleteBtn.className = "delete";
-// //     listItem.appendChild(deleteBtn);
-  
-// //     var checkBox = listItem.querySelector("input[type=checkbox]");
-// //     checkBox.remove();
-  
-// //     completeUl.appendChild(listItem);
-  
-// //     bindCompleteItems(listItem, deleteTask);
-// // };
-
-// function createCompleteTask() {
-//     let listItem = this.parentNode;
-//     //const listItem = document.getElementById("complete-ul-list");
-//     const deleteBtn = document.createElement("button");
-//     deleteBtn.innerText ="Delete"; 
-//     deleteBtn.className = "delete-button";
-//     listItem.appendChild(deleteBtn);
-    
-//     //SELECT THE CHECKBOX FROM THE COMPLETED CHECKBOX AND REMOVE IT
-//     var checkBox = listItem.querySelector("input[type=checkbox]");
-//     checkBox.remove();
-    
-//     //PLACE IT INSIDE THE COMPLETED LIST
-//     completeTask.appendChild(listItem); 
-    
-//     //BIND THE NEW COMPLETED LIST
-//     bindCompleteItems(listItem, removeItem());
-    
-// };
-
-// function removeItem() {
-//     const item = this.parentNode;
-//     const ul = item.parentNode;
-    
-//     ul.removeChild(item);
-// };
-
-// function bindIncompleteItems(taskItem, checkBoxClick) {  
-//     const checkBox = taskItem.getElementsByClassName("status");
-//     checkBox.onchange = checkBoxClick;  
-// }; 
-
-// function bindCompleteItems(taskItem, deleteButtonPress){
-//     const deleteButton = taskItem.getElementsByClassName("delete-button");
-//     deleteButton.onclick = deleteButtonPress;  
-// };
-
-// for(var i=0; i < incompleteTask.children.length; i++) {
-//   bindIncompleteItems(incompleteTask.children[i], createCompleteTask());
-// }
-
-// for(var i=0; i < completeTask.children.length; i++) {
-//   bindCompleteItems(completeTask.children[i], removeItem());
-// }
-
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Add highlight effect to checkbox
-// function myFunction(x, _this) {
-//     x.style.color = _this.checked ? '#00BFFF' : '#000000';
-//     x.style.textDecoration =  _this.checked ? 'line-through' : 'none';
-//     x.style.display =  _this.checked ? 'none' : 'block';
-//     x.classList.toggle('blue');
-// }
-
-// function highlight(){
-//     document.getElementsByClassName('li-list').style.background = "#e6e6e6";
-// }
-
-// Show current date and time at the top of the page
+function ac(value) { 
+  console.log("Enable autocomplete...")
+  document.getElementById('datalist').innerHTML = ''; 
+  // console.log(tasks)
+  // console.log(tasks.length)
+  l=value.length; 
+  for (var i = 0; i<tasks.length; i++) { 
+    if(((tasks[i].toLowerCase()).indexOf(value.toLowerCase()))>-1) { 
+        var node = document.createElement("option"); 
+        var val = document.createTextNode(tasks[i]); 
+        node.appendChild(val); 
+        document.getElementById("datalist").appendChild(node); 
+        } 
+    } 
+} 
